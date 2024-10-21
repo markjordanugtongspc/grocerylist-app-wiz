3524bdbd3f0e5a523a20eba434e93c4d8bf155f6
markjordanugtongspc <markjordan.ugtong.202200752@gmail.com>
2024-10-22 01:22:51 +0800

    devlogs: created a clone and used simple and basic React.js and JavaScript functions

---
+++ Z-CONVERTED/grocerylist/src/components/Footer.js
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <span>Copyright &copy; 2024</span>
+    </div>
+  );
+}
+
+export default Footer;
